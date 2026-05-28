e75a5d8bfb2d381d3652c888b148fa08dca01897
Shreyansh Singh <shreyanshsingh0346@gmail.com>
2026-05-28 17:54:57 +0530

    update

---
+++ Day1/Q1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,sum=0;
+    printf("Enter a number");
+    scanf("%d" ,&n);
+    for(i=1;i<=n;i++)
+    {
+      sum = sum + i;  
+    }
+    printf("sum of first %d natural numbers = %d" ,n,sum);
+
+    return 0;
+}
