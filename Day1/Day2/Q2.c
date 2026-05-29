59b4d63211404cd364cd0387f86bc767f98af7cf
Shreyansh Singh <shreyanshsingh0346@gmail.com>
2026-05-29 20:51:32 +0530

    update

---
+++ Day1/Day2/Q2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n,d,rev=0;
+    printf("Enter the number");
+    scanf("%d" ,&n);
+    while(n>0){
+        d=n%10;
+        rev=rev*10+d;
+        n=n/10;
+    }
+    printf("The reverse number is =%d" ,rev);
+    return 0;
+
+}
