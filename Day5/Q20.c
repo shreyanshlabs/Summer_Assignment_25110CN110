8d663f338acde3e5c904945c8a3c57ae9845eea2
Shreyansh Singh <shreyanshsingh0346@gmail.com>
2026-06-05 18:57:01 +0530

    Day5 update

---
+++ Day5/Q20.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    long long n;
+    long long largestPrime = -1;
+
+    printf("Enter a number: ");
+    scanf("%lld", &n);
+
+    while(n % 2 == 0) {
+        largestPrime = 2;
+        n /= 2;
+    }
+
+    for(long long i = 3; i * i <= n; i += 2) {
+        while(n % i == 0) {
+            largestPrime = i;
+            n /= i;
+        }
+    }
+
+    if(n > 2)
+        largestPrime = n;
+
+    printf("Largest Prime Factor = %lld", largestPrime);
+
+    return 0;
+}
