8397fc036adc115574003a0e9f26d492ae9969f5
Shreyansh Singh <shreyanshsingh0346@gmail.com>
2026-06-10 12:21:54 +0530

    Day8 update

---
+++ Day8/Q31.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+int main() {
+    int n = 5;
+    for(int i = 1; i <= n; i++) {
+        for(char ch = 'A'; ch < 'A' + i; ch++) {
+            printf("%c", ch);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
